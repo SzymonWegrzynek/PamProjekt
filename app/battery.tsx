5000d71d5460fdb2b685073c37df2d0720a30a08
Kajakkkkkk <u100_KajDej_waw@technischools.com>
2025-04-04 16:34:26 +0200

    battery.tsx

--- app/battery.tsx
+++ app/battery.tsx
@@ -1,14 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import {
+  useBatteryLevel,
+  useBatteryState,
+  BatteryState,
+} from "expo-battery";
 
-const battery = () => {
+const BatteryStatus = () => {
+  const batteryLevel = useBatteryLevel();
+  const batteryState = useBatteryState();
+
+  const getBatteryStatusText = () => {
+    if (batteryLevel === null || batteryState === null)
+      return "Pobieranie danych...";
+
+    if (batteryLevel >= 0.99) return " Pełna bateria";
+    if (batteryState === BatteryState.CHARGING) return " Ładowanie";
+    return "Rozładowywanie";
+  };
+
   return (
-    <View>
-      <Text>battery</Text>
+    <View >
+      <Text >
+        Poziom baterii:{" "}
+        {batteryLevel !== null
+          ? `${(batteryLevel * 100).toFixed(0)}%`
+          : "Ładowanie..."}
+      </Text>
+      <Text>Stan baterii: {getBatteryStatusText()}</Text>
     </View>
   );
 };
 
-export default battery;
 
-const styles = StyleSheet.create({});
+export default BatteryStatus;
