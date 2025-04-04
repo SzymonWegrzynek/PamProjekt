7e811eb9b254c04eb0b84a01e142e44dc941df08
Aleksander Piotrowski <aleksanderpiotrowski194@gmail.com>
2025-04-04 18:02:39 +0200

    dodałem cssa ale pewnie jeszcze będzie zmieniony + dodałem getStatusColor

--- app/battery.tsx
+++ app/battery.tsx
@@ -14,23 +14,45 @@ const BatteryStatus = () => {
     if (batteryLevel === null || batteryState === null)
       return "Pobieranie danych...";
 
-    if (batteryLevel >= 0.99) return " Pełna bateria";
-    if (batteryState === BatteryState.CHARGING) return " Ładowanie";
+    if (batteryLevel >= 0.99) return "Pełna bateria";
+    if (batteryState === BatteryState.CHARGING) return "Ładowanie";
     return "Rozładowywanie";
   };
 
+  const getStatusColor = (status: string) => {
+    switch (status) {
+      case "Ładowanie":
+        return "#2dc653";
+      case "Rozładowywanie":
+        return "red";
+      case "Pełna bateria":
+        return "#2dc653";
+      default:
+        return "#fffcf2";
+    }
+  };
+
+  const batteryStatusText = getBatteryStatusText();
+
   return (
-    <View >
-      <Text >
+    <View style={{ backgroundColor: '#0a0908', width: '90%', borderRadius: 10, height: 100, alignSelf: 'center' }}>
+      <Text style={{ color: '#fffcf2', fontWeight: 'bold', fontSize: 28, marginLeft: 5 }}>Bateria</Text>
+
+      <Text style={{ color: '#fffcf2', marginLeft: 5 }}>
         Poziom baterii:{" "}
-        {batteryLevel !== null
-          ? `${(batteryLevel * 100).toFixed(0)}%`
-          : "Ładowanie..."}
+        <Text style={{ fontWeight: 'bold' }}>
+          {batteryLevel !== null ? `${(batteryLevel * 100).toFixed(0)}%` : "Ładowanie..."}
+        </Text>
       </Text>
-      <Text>Stan baterii: {getBatteryStatusText()}</Text>
+
+      <Text style={{ color: '#fffcf2', marginLeft: 5 }}>
+        Stan baterii:{" "}
+        <Text style={{ fontWeight: 'bold', color: getStatusColor(batteryStatusText) }}>
+          {batteryStatusText}
+        </Text>
+      </Text>
     </View>
   );
 };
-
 
 export default BatteryStatus;
